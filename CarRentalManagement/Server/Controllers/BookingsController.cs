18112af5fb930091284cf4b7a72a90187e25f485
TAN CAI ZHI 2100792G <2100792G@student.tp.edu.sg>
2022-12-01 15:53:28 +0800

    Added Blazor List Pages with Delete function

--- CarRentalManagement/Server/Controllers/BookingsController.cs
+++ CarRentalManagement/Server/Controllers/BookingsController.cs
@@ -36,7 +36,7 @@ namespace CarRentalManagement.Server.Controllers
         {
             //Refactored
             //return await _context.Bookings.ToListAsync();
-            var bookings = await _unitOfWork.Bookings.GetAll();
+            var bookings = await _unitOfWork.Bookings.GetAll(includes: q => q.Include(x => x.Vehicle).Include(x => x.Customer));
             return Ok(bookings);
         }
 
